e686d299fd963e10e1c2b3ecc86be49276edeeee
shiv4654u <59159411+shiv4654u@users.noreply.github.com>
2020-02-25 11:07:48 +0530

    crud

--- app/Teams.php
+++ app/Teams.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teams extends Model
 {
+    protected $fillable = ['name','logoUri'];
+    
     public function players(){
         return $this->hasMany(Teams::class);
     }
