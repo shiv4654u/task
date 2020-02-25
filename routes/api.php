cf7cc9f47fd8a3fd13ed85de627a0f0210bf28cf
shiv4654u <59159411+shiv4654u@users.noreply.github.com>
2020-02-25 11:09:05 +0530

    crud

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/teams/create', 'TeamsController@store');
+Route::get('/teams/edit/{id}', 'TeamsController@edit');
 Route::get('/teams/players/{id}', 'TeamsController@players');
+Route::post('/teams/update/{id}', 'TeamsController@update');
+Route::delete('/teams/delete/{id}', 'TeamsController@delete');
 Route::get('/teams/matches', 'TeamsController@matches');
 Route::get('/teams/points', 'TeamsController@points');
 Route::get('/teams', 'TeamsController@index');
